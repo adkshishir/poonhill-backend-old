77d41d076083b644cf9a0b31870e7ba9c2225425
adkshishir <98789290+adkshishir@users.noreply.github.com>
2024-04-12 07:27:25 +0545

    Install Adminlte

--- database/migrations/2024_03_22_164717_create_seos_table.php
+++ database/migrations/2024_03_22_164717_create_seos_table.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('seos', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('activity_id')->constrained();
+            $table->foreignId('activity_id')->nullable()->constrained();
+            $table->foreignId('destination_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('parent_activity_id')->nullable()->constrained()->cascadeOnDelete();
             $table->string('meta_title')->nullable();
             $table->string('meta_description')->nullable();
             $table->string('meta_keywords')->nullable();
             $table->string('canonical_url')->nullable();
             $table->string('schema')->nullable();
+            $table->string("og_title")->nullable();
+            $table->string('og_description')->nullable();
+            $table->string("og_type")->nullable();
             $table->timestamps();
         });
     }
